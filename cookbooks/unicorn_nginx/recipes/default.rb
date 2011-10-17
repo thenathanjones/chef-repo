547c77db91b9a3088c621d33083e80bbe79a0457
Nathan Jones <thenathanjones@gmail.com>
2011-10-17 14:02:38 +0800

    Chef scripts that were used to create working example application

--- cookbooks/unicorn_nginx/recipes/default.rb
+++ cookbooks/unicorn_nginx/recipes/default.rb
@@ -49,15 +49,23 @@ template "#{node[:nginx][:dir]}/conf.d/upstream.conf" do
   mode 0644
 end
 
+# ensure the log directory for unicorn is created
+unicorn_log_dir = "/var/log/unicorn"
+directory "#{unicorn_log_dir}" do
+  mode 0755
+  owner "#{node[:nginx][:user]}"
+  group "#{node[:nginx][:group]}"
+end
+
 # create a unicorn config to match
 port_setup = {"'/tmp/.sock'" => {:backlog => 64}}
-deploy_dir = "#{node[:nginx][:working_dir]}/#{node[:app_name]/current"
-unicorn_config "#{deploy_dir}/conf/unicorn.rb" do
+deploy_dir = "#{node[:nginx][:working_dir]}/#{node[:app_name]}/current"
+unicorn_config "#{deploy_dir}/config/unicorn.rb" do
   working_directory "#{deploy_dir}"
   listen port_setup
   owner "#{node[:nginx][:user]}"
   group "#{node[:nginx][:group]}"
   pid "/var/run/unicorn.pid"
-  stderr_path "/var/log/unicorn/error.log"
-  stdout_path "/var/log/unicorn/out.log"
+  stderr_path "#{unicorn_log_dir}/error.log"
+  stdout_path "#{unicorn_log_dir}/out.log"
 end
